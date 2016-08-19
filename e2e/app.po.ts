2ec060d33fa9a90b10782c3b61a4cf7de38e42b0
KuldeepDesai <me.kuldeep.desai@gmail.com>
2016-08-19 20:22:10 +0530

    moving to ng 1

--- e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class MhvresidentsPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('mhvresidents-app h1')).getText();
-  }
-}
--- src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export {environment} from './environment';
-export {MhvresidentsAppComponent} from './mhvresidents.component';
